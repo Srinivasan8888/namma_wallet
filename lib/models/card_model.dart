00fd43f86b4932132dd0290abf8416263813c8aa
KeerthiVasan-ai <keerthi77459@gmail.com>
2025-09-11 22:46:00 +0530

    fix: missing params in the conversion from TNSTCModel to GenericDetailModel

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -1,35 +1,36 @@
 // ignore_for_file: public_member_api_docs
 
-
 import 'package:namma_wallet/models/tnstc_model.dart' show TNSTCModel;
 
-enum Type { event, busTicket, trainTicket, generic,none}
+enum Type { event, busTicket, trainTicket, generic, none }
 
 class GenericDetailsModel {
- final String primaryText;
- final String secondaryText;
- final String? discription;
- final DateTime startTime;
- final DateTime? endTime;
- final String location;
- final Type type;
- final Map<String,dynamic>?extras;
-   GenericDetailsModel({
-    required this.primaryText,
-    required this.secondaryText,
-    required this.startTime,
-    required this.location,
-     this.type=Type.none,
-    this.endTime,
-    this.extras,
-    this.discription
-  });
+  final String primaryText;
+  final String secondaryText;
+  final String? discription;
+  final DateTime startTime;
+  final DateTime? endTime;
+  final String location;
+  final Type type;
+  final Map<String, dynamic>? extras;
+  GenericDetailsModel(
+      {required this.primaryText,
+      required this.secondaryText,
+      required this.startTime,
+      required this.location,
+      this.type = Type.none,
+      this.endTime,
+      this.extras,
+      this.discription});
 
-  GenericDetailsModel.fromTrainTicket(TNSTCModel ticket):
-    primaryText='${ticket.from} → ${ticket.to}',
-    secondaryText='${ticket.corporation} → ${ticket.service}',
-    startTime=DateTime.parse(ticket.journeyDate),
-    location=ticket.boardingAt,
-    type=Type.trainTicket,
-    extras=ticket.toJson(); // to be fixed
+
+  // TODO(Praveen): Please review this
+  GenericDetailsModel.fromTrainTicket(
+      TNSTCModel ticket, this.discription, this.endTime)
+      : primaryText = '${ticket.from} → ${ticket.to}',
+        secondaryText = '${ticket.corporation} → ${ticket.service}',
+        startTime = DateTime.parse(ticket.journeyDate),
+        location = ticket.boardingAt,
+        type = Type.trainTicket,
+        extras = ticket.toMap(); // to be fixed
 }
