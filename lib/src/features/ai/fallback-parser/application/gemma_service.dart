f411320e264f4995b452c28aacc99c8beef959c3
Praveen-Pandiyan <pravinand.io@gmail.com>
2025-11-05 23:34:05 +0530

    chore: adding HF token in config

--- lib/src/features/ai/fallback-parser/application/gemma_service.dart
+++ lib/src/features/ai/fallback-parser/application/gemma_service.dart
@@ -6,18 +6,22 @@ class GemmaChatService {
   //https://huggingface.co/litert-community/Gemma3-1B-IT/resolve/main/Gemma3-1B-IT_multi-prefill-seq_q4_block128_ekv4096.task
 
   Future<void> init() async {
+    const token = String.fromEnvironment('HUGGINGFACE_TOKEN');
+    if (token.isEmpty) {
+      throw Exception(
+        'HuggingFace token is not set. Please provide a valid token.',
+      );
+    }
     await FlutterGemma.installModel(
           modelType: ModelType.gemmaIt,
         )
         .fromNetwork(
           'https://huggingface.co/litert-community/Gemma3-1B-IT/resolve/main/Gemma3-1B-IT_multi-prefill-seq_q4_ekv2048.task',
-          token: 'hf_xxxYourHuggingFaceTokenxxx',
+          token: token,
         )
         .withProgress((progress) {
           print('Downloading: ${progress}%');
         })
         .install();
   }
-
-
 }
