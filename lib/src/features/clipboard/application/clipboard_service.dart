47c45fc017e811820ce27d04bfcf19bf8880696e
harishwarrior <warriorharish95668@gmail.com>
2025-11-08 10:29:26 +0530

    fix: hardcoded theme

--- lib/src/features/clipboard/application/clipboard_service.dart
+++ lib/src/features/clipboard/application/clipboard_service.dart
@@ -195,7 +195,7 @@ class ClipboardService {
               : 'Ticket updated with conductor details!',
         ClipboardContentType.invalid => 'Invalid content',
       };
-      backgroundColor = const Color(0xff3067FE);
+      backgroundColor = Theme.of(context).colorScheme.primary;
 
       _logger.success('Clipboard operation succeeded: $message');
     } else {
