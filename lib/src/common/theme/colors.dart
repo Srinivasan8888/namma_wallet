18e1c0242e85622894ae510a892954d1538cb57a
harishwarrior <warriorharish95668@gmail.com>
2025-11-08 10:23:19 +0530

    chore: removed unused theme

--- lib/src/common/theme/colors.dart
+++ lib/src/common/theme/colors.dart
@@ -8,20 +8,8 @@ class AppColor {
   static const Color specialColor = Color.fromARGB(255, 236, 241, 248);
   static const Color periwinkleBlue = Color.fromRGBO(112, 135, 248, 1);
 
-  /// Primary blue color used throughout the app
-  /// This is the actual blue color (0xff3067FE), previously
-  /// misnamed as lime yellow
+  /// Primary blue color used throughout the app (0xff3067FE)
   static const Color primaryBlue = Color(0xff3067FE);
-
-  /// @deprecated Use [primaryBlue] instead. This was misnamed
-  /// - it's blue, not yellow.
-  @Deprecated('Use primaryBlue instead. This constant is misnamed.')
-  static const Color limeYellowColor = primaryBlue;
-
-  /// @deprecated Use [primaryBlue] instead. This was misnamed
-  /// - it's blue, not yellow.
-  @Deprecated('Use primaryBlue instead. This constant is misnamed.')
-  static const Color limeYellowThinkColor = primaryBlue;
 
   static const Color whiteColor = Colors.white;
   static const Color blackColor = Colors.black;
